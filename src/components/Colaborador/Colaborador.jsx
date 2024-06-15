4504e7b037cc35ace3d18430ab11832334ec3c4b
emaenriquez1985 <emaenriquez1985@gmail.com>
2024-06-15 03:30:42 +0000

    colaboradores

---
+++ src/components/Colaborador/Colaborador.jsx
@@ -0,0 +1,16 @@
+
+import './Colaborador.css'
+
+function Colaborador(props) {
+    return <div className='colaborador'>
+        <div className='encabezado'>
+            <img src="https://github.com/emaenriquez.png" alt='emaenriquez'></img>
+        </div>
+        <div className='info'>
+            <h4>Emanuel Enriquez</h4>
+            <h5>Desarrollador web</h5>
+        </div>
+    </div>    
+}
+
+export default Colaborador
--- src/components/Equipo/Equipo.jsx
+++ src/components/Equipo/Equipo.jsx
@@ -1,4 +1,5 @@
 
+import Colaborador from '../Colaborador/Colaborador'
 import './Equipo.css'
 
 function Equipo(props) {
@@ -15,7 +16,9 @@ function Equipo(props) {
 
     return <section className="equipo" style={obj}>
         <h3 style={estiloTitulo}>{titulo}</h3>
-        <div className="colaboradores"></div>
+        <div className="colaboradores">
+            <Colaborador></Colaborador>
+        </div>
     </section>
 }
 
